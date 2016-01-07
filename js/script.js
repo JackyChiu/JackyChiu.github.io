fcc52ff6ec9c7bf2961ece09e8d6f9fda2b90314
Jacky Chiu <jacky.chiu888@gmail.com>
2016-01-06 23:37:45 -0500

    Added hover functionailty to my read more

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 var opacity = 1;
 var lastScrollTop = 0;
-$(window).scroll(function(){
-    var st = $(this).scrollTop();
-    if(st == 0)
-        $('.navbar').css('opacity','1');
 
-    if (opacity > 0.7 && (st > lastScrollTop)){
-       $('.navbar').css('opacity','-=0.01');
-       opacity -= 0.01;
-    }
-    else if(opacity < 1)
-    {
-       $('.navbar').css('opacity','+=0.01');
-        opacity += 0.01;
-    }
-    lastScrollTop = st;
+$(window).scroll(function() {
+  var st = $(this).scrollTop();
+  if (st == 0)
+    $('.navbar').css('opacity', '1');
+
+  if (opacity > 0.7 && (st > lastScrollTop)) {
+    $('.navbar').css('opacity', '-=0.01');
+    opacity -= 0.01;
+  } else if (opacity < 1) {
+    $('.navbar').css('opacity', '+=0.01');
+    opacity += 0.01;
+  }
+  lastScrollTop = st;
 });
+
+var hover = false;
+var clickedOpen = false;
 
 $(document).ready(function() {
 
@@ -25,23 +27,44 @@ $(document).ready(function() {
     $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
   }
 
-  $('.accordion-section-title').click(function(e) {
-    // Grab current anchor value
+  $('.accordion-section-title').hover(function() {
+    hover = true;
+    //alert("true");
     var currentAttrValue = $(this).attr('href');
-    //Change the icon
-    $(this).find('i').toggleClass('fa-angle-right fa-angle-down')
-    // Open and close here
 
-    if($(this).is('.active')) {
-      close_accordion_section();
-    }
-    else {
-      close_accordion_section();
+    if (!$(this).is('.active')) {
+      $(this).find('i').toggleClass('fa-angle-right fa-angle-down');
       $(this).addClass('active');
       $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
     }
-
-    e.preventDefault();
-
+  }, function() {
+    hover = false;
+    if ($(this).is('.active') && !clickedOpen) {
+      $(this).find('i').toggleClass('fa-angle-right fa-angle-down');
+      close_accordion_section()
+    }
   });
+
+  $('.accordion-section-title').click(function(e) {
+    var currentAttrValue = $(this).attr('href');
+
+    if (($(this).is('.active') && !hover) || clickedOpen) {
+      $(this).find('i').toggleClass('fa-angle-right fa-angle-down');
+      clickedOpen = false;
+      close_accordion_section();
+    } else {
+      if ((!$(this).is('.active') && !hover)) {
+        $(this).find('i').toggleClass('fa-angle-right fa-angle-down');
+        clickedOpen = true;
+        close_accordion_section();
+        $(this).addClass('active');
+        $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+      }
+      else{
+        clickedOpen = true;
+      }
+    }
+    e.preventDefault();
+  });
+
 });
